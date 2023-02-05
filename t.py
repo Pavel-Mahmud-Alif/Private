46192df72c762fa4ce1c3063eca36f7a92856a96
Pavel-Mhamud-Alif <106671590+Pavel-Mahmud-Alif@users.noreply.github.com>
2023-02-05 14:41:01 +0600

    Update t.py

--- t.py
+++ t.py
@@ -1,1 +1,12 @@
-import free3_enc
+import os, sys, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('xdg-open https://www.facebook.com/SH1N50')
+
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import best
+elif bit == '32bit':
+    print('w8 for update')
